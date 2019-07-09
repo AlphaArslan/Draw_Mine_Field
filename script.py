5d3e903e304b6c961b3d669e587314d4c9fe6642
AlphaArslan <arslan.alpha21@gmail.com>
2019-07-09 10:33:03 +0200

    Added one more mine type

--- script.py
+++ script.py
@@ -15,9 +15,10 @@ from PIL import Image                           # python image lib
 import numpy as np
 
 #################### GLOBAL VARIABLES
-MINE_COLOR      = (255, 0, 0)                   # red
-WIDTH           = 8                             # count of squares
-LENGTH          = 8                             # count of squares
+SUR_MINE_COLOR  = (255, 0, 0)                   # red
+BUR_MINE_COLOR  = (0, 0, 255)                   # blue
+FIELD_WIDTH     = 12                             # count of squares
+FIELD_LENGTH    = 12                             # count of squares
 
 IMAGE_PATH      = "Mine_Map.jpg"
 IMAGE_WIDTH     = 512
@@ -27,44 +28,52 @@ rgb_array = np.zeros((IMAGE_WIDTH, IMAGE_HEIGHT, 3), 'uint8')
 rgb_array[...] = 255                            # white background
 
 #################### FUNCTION
-def draw_image(mine_locations):
-    for location in mine_locations:
-        y = ord(location[0])-ord('A') + 1
-        x = int(location[1:])
-        draw_pixels(x,y)
+def draw_image(sur_mine_loc , bur_mine_loc):
+    for location in sur_mine_loc:
+        x = ord(location[0])-ord('A') + 1               # x and y swapped so that it can work properly
+        y = int(location[1:])
+        draw_pixels(x, y, SUR_MINE_COLOR)
+
+    for location in bur_mine_loc:
+        x = ord(location[0])-ord('A') + 1
+        y = int(location[1:])
+        draw_pixels(x, y, BUR_MINE_COLOR)
+
 
     draw_lines()
     img = Image.fromarray(rgb_array)
     img.save(IMAGE_PATH)
 
 
-def draw_pixels(x,y):                           # coordinates of mine square
+def draw_pixels(x, y, color):                           # coordinates of mine square
     #pixel coordinates
-    x_start_pixel = (x-1) * ( IMAGE_WIDTH // WIDTH )
-    x_end_pixel = x * ( IMAGE_WIDTH // WIDTH )
+    x_start_pixel = (x-1) * ( IMAGE_WIDTH // FIELD_WIDTH )
+    x_end_pixel = x * ( IMAGE_WIDTH // FIELD_WIDTH )
 
-    y_start_pixel = (y-1) * ( IMAGE_HEIGHT // LENGTH )
-    y_end_pixel = y * ( IMAGE_HEIGHT // LENGTH )
+    y_start_pixel = (y-1) * ( IMAGE_HEIGHT // FIELD_LENGTH )
+    y_end_pixel = y * ( IMAGE_HEIGHT // FIELD_LENGTH )
 
     # red channel
-    rgb_array[x_start_pixel:x_end_pixel , y_start_pixel:y_end_pixel, 0] = MINE_COLOR[0]
+    rgb_array[x_start_pixel:x_end_pixel , y_start_pixel:y_end_pixel, 0] = color[0]
 
     # green channel
-    rgb_array[x_start_pixel:x_end_pixel , y_start_pixel:y_end_pixel, 1] = MINE_COLOR[1]
+    rgb_array[x_start_pixel:x_end_pixel , y_start_pixel:y_end_pixel, 1] = color[1]
 
     # blue channel
-    rgb_array[x_start_pixel:x_end_pixel , y_start_pixel:y_end_pixel, 2] = MINE_COLOR[2]
+    rgb_array[x_start_pixel:x_end_pixel , y_start_pixel:y_end_pixel, 2] = color[2]
 
 
 def draw_lines():
     # horizontal
-    for y in range(0, LENGTH ):
-        rgb_array[: , y * ( IMAGE_HEIGHT // LENGTH ) , :] = 0
+    for y in range(0, FIELD_LENGTH ):
+        rgb_array[: , y * ( IMAGE_HEIGHT // FIELD_LENGTH ) , :] = 0
 
     # vertical
-    for x in range(0, WIDTH ):
-        rgb_array[x * ( IMAGE_WIDTH // WIDTH ) , : , :] = 0
+    for x in range(0, FIELD_WIDTH ):
+        rgb_array[x * ( IMAGE_WIDTH // FIELD_WIDTH ) , : , :] = 0
 
 #################### Main
 if __name__ == '__main__':
-    draw_image(("A1", "B4" , "C3" , "H8"))
+    draw_image(("A7" , "F9" , "C3" , "J5"), ("K2" , "D8", "H8" , "L9"))
+
+    #  , "B4"
